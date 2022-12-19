6c02ca5b4818a520912bd74abe5beb5d669bb405
Jacob YC <jacob.yc.wang@foxconn.com>
2022-12-19 22:22:00 +0800

    Initial Commit

---
+++ GPSSpoof/GPSSpoofApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPSSpoofApp.swift
+//  GPSSpoof
+//
+//  Created by 王予智 on 2022/12/19.
+//
+
+import SwiftUI
+
+@main
+struct GPSSpoofApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GPSSpoofUITests/GPSSpoofUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GPSSpoofUITestsLaunchTests.swift
+//  GPSSpoofUITests
+//
+//  Created by 王予智 on 2022/12/19.
+//
+
+import XCTest
+
+final class GPSSpoofUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
